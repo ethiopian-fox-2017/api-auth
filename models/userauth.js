6960347922bd4ec9a404e3d1e2eaca25943f8dcd
Anthony Juan <anthonyjuan95@gmail.com>
2017-03-27 18:33:07 +0700

    username isunique

--- models/userauth.js
+++ models/userauth.js
@@ -1,7 +1,23 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Userauth = sequelize.define('Userauth', {
-    username: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        isUnique: function(value,next) {
+          Userauth.find({where:{username:value}})
+                  .then((user) => {
+                    if(user) {
+                      return next('Username already in use');
+                    }
+                    return next();
+                  })
+                  .catch((err) => {
+                    return next(err.message)
+                  })
+        }
+      }
+    },
     password: DataTypes.STRING,
     role: DataTypes.STRING
   }, {
