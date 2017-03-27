55520e82ec7865ef9e485d504c2bdb056e27a8a5
andrasat <andra.satria1@gmail.com>
2017-03-27 17:33:29 +0700

    added username unique validation

--- models/user.js
+++ models/user.js
@@ -3,7 +3,23 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     firstname: DataTypes.STRING,
     lastname: DataTypes.STRING,
-    username: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        isUnique: (val, cb)=> {
+          User.find({
+            where : {username: val},
+            attributes: ['id']
+          }).done((err)=> {
+            if(err) {
+              return cb('Username already in use')
+            } else {
+              cb()
+            }
+          })
+        }
+      }
+    },
     password: DataTypes.STRING,
     roleAdmin: DataTypes.BOOLEAN
   }, {
