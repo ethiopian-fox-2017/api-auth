b421182ee7fb4d60e5fd4519302d79bdbf085ce7
zidane9 <d4rr@hotmail.com>
2017-03-28 12:41:30 +0700

    add username validation

--- models/teacher.js
+++ models/teacher.js
@@ -2,7 +2,22 @@
 module.exports = function(sequelize, DataTypes) {
   var Teacher = sequelize.define('Teacher', {
     name: DataTypes.STRING,
-    username: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        isUnique: function(value,next){
+          Teacher.find({
+            where: {username: value}
+          }).done((exist) => {
+            if(exist){
+              console.log('user name already in use!');
+            } else {
+              next();
+            }
+          })
+        }
+      }
+    },
     password: DataTypes.STRING,
     role: DataTypes.STRING
   }, {
